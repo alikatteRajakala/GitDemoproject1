42ce63bf87b3a399d7d04cf66ca891aaf7b71405
Dell <Dell@DESKTOP-AUE88L6>
2023-05-16 14:46:15 +0530

    "try untill sucess arrives"

---
+++ src/TEST/Test1Java.java
@@ -0,0 +1,15 @@
+package TEST;
+
+public class Test1Java {
+	public static void main(String[] args) {
+		String name="raja";
+		if(name=="raja") {
+			System.out.println("auyhemticated...");
+			
+		}
+		else {
+			System.out.println("Unauthenticated");
+		}
+	}
+
+}
